cc780b319dc1beb0530f7edd8b3966cc3fd2b98f
abhi <audreygallier@outlook.com>
2020-04-30 16:46:33 -0400

    hola

--- array.c
+++ array.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+void change();
+double distance(double a[2], double b[2]);
 
 // define void function to dynamically allocate array
 void push(double *arr, int ind){
@@ -8,9 +12,10 @@ void push(double *arr, int ind){
 
 // define global variables
 double step = 1;
-double l = 1;
-double a[2];
-double b[2];
+double a[2] = {0, 0};
+double b[2] = {1, 0};
+double u = 2;
+double v = 3;
 
 int main () {
 	// define dynamic array to store historical data
@@ -19,8 +24,11 @@ int main () {
 	// using flag technique
 	long flag = 0;
 	while (flag != -1){
+
+    printf("%lf\n", distance(a, b));
+
 		// update vectors
-		change(a, b);
+		change();
 
 		// dynamically allocate more space in history 
 		push(hist, flag);
@@ -34,7 +42,7 @@ int main () {
 
 		// flag and raise if necessary
 		flag++;
-		if (dist(a, b) < step) {
+		if (distance(a, b) < (step* (v - u))) {
 			flag = -1;
 		}
 		
@@ -42,3 +50,42 @@ int main () {
 
 	return 0;
 }
+
+double distance(double a[2], double b[2]) {
+  double result, sq1, sq2;
+  double dx = fabs(a[0] - b[0]);
+  double dy = fabs(a[1] - b[1]);
+  result = sqrt((dx * dx) + (dy * dy));
+  return result;
+}
+
+void change() {
+  double dx = b[0] - a[0];
+  double dy = b[1] - a[1];
+  double hypotenuse = distance(a, b);
+
+  a[0] += (dx / hypotenuse) * step * v;
+  a[1] += (dy / hypotenuse) * step * v;
+  b[1] += step * u;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
